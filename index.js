ad3a32e54209df06c953c994f29de137cd0fc06c
SOURAV ROY <souravroy.info13@gmail.com>
2020-11-15 21:13:53 +0600

    Page Navigate Success ✔💚

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ let fs = require('fs');
 
 let server = http.createServer(function (req, res) {
 
+    if (req.url === "/") {
+        let home = fs.readFileSync("home.html", 'utf-8');
+        res.end(home);
+
+    } else if (req.url === "/about") {
+        let About = fs.readFileSync("About.html", 'utf-8');
+        res.end(About);
+
+    } else if (req.url === "/contact") {
+        let Contact = fs.readFileSync("Contact.html", 'utf-8');
+        res.end(Contact);
+
+    } else if (req.url === "/portfolio") {
+        let Portfolio = fs.readFileSync("Portfolio.html", 'utf-8');
+        res.end(Portfolio);
+
+    }
 });
 
 server.listen(7070);
