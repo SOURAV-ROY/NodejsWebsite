649f13110bddc99f716012af25f747254712bd8a
SOURAV ROY <souravroy.info13@gmail.com>
2020-11-17 14:49:18 +0600

    mysql Connect With NodeJS

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-let http = require('http');
-let fs = require('fs');
+// let http = require('http');
+// let fs = require('fs');
+//
+// let server = http.createServer(function (req, res) {
+//
+//     if (req.url === "/") {
+//         let home = fs.readFileSync("home.html", 'utf-8');
+//         res.end(home);
+//
+//     } else if (req.url === "/about") {
+//         let About = fs.readFileSync("About.html", 'utf-8');
+//         res.end(About);
+//
+//     } else if (req.url === "/contact") {
+//         let Contact = fs.readFileSync("Contact.html", 'utf-8');
+//         res.end(Contact);
+//
+//     } else if (req.url === "/portfolio") {
+//         let Portfolio = fs.readFileSync("Portfolio.html", 'utf-8');
+//         res.end(Portfolio);
+//
+//     }
+// });
+//
+// server.listen(7070);
+// console.log("Serer 7070 RUN Success!!");
 
-let server = http.createServer(function (req, res) {
+let mysql = require("mysql");
 
-    if (req.url === "/") {
-        let home = fs.readFileSync("home.html", 'utf-8');
-        res.end(home);
+let DBC = {
+    host: "localhost",
+    user: "root",
+    password: "",
+}
 
-    } else if (req.url === "/about") {
-        let About = fs.readFileSync("About.html", 'utf-8');
-        res.end(About);
+let DBConnect = mysql.createConnection(DBC);
 
-    } else if (req.url === "/contact") {
-        let Contact = fs.readFileSync("Contact.html", 'utf-8');
-        res.end(Contact);
-
-    } else if (req.url === "/portfolio") {
-        let Portfolio = fs.readFileSync("Portfolio.html", 'utf-8');
-        res.end(Portfolio);
-
+DBConnect.connect(function (error) {
+    if (error) {
+        console.log("Connection Fail");
+    } else {
+        console.log("Connection Success !!!")
     }
-});
-
-server.listen(7070);
-console.log("Serer 7070 RUN Success!!");
+})
